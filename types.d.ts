b8dd91bf62dfeb54ca3ad80b688ace6589bcd03e
pablitomito <111746731+pablitomito@users.noreply.github.com>
2026-02-27 17:32:39 +0000

    Erros

---
+++ types.d.ts
@@ -0,0 +1,7 @@
+// types.d.ts
+declare module 'strip-json-comments';
+declare module 'yauzl';
+declare module '@babel/core';
+declare module '@babel/generator';
+declare module '@babel/template';
+declare module '@babel/traverse';
